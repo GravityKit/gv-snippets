4e04a08d58f9c7369eb6d46a3aa2a41dea037f39
Zack Katz <zack@katzwebservices.com>
2015-06-25 12:23:18 -0600

    GravityView Mod: Exact Match Search

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name:       GravityView Mod: __description__
- * Plugin URI:        https://github.com/katzwebservices/gv-snippets/tree/__ID__
- * Description:       __description__
+ * Plugin Name:       GravityView Mod: Exact Match Search
+ * Plugin URI:        https://github.com/katzwebservices/gv-snippets/tree/2730-use-exact-match
+ * Description:       Exact match search results
  * Version:           1.0
  * Author:            GravityView
  * Author URI:        https://gravityview.co
@@ -15,23 +15,17 @@ if ( ! defined( 'WPINC' ) ){
 	die;
 }
 
-class GV_Snippet___ID__ {
+class GV_Snippet_2730_3 {
 
-	public static $ID = __ID__;
-
-	private static $_instance = null;
-
-	public static function instance(){
-		if ( ! ( self::$_instance instanceof self ) ) {
-			self::$_instance = new self();
-		}
-
-		return self::$_instance;
-	}
-
-	public function __construct(){
-
+	/**
+	 * @param string $operator Existing operator; "like" by default
+	 * @param array $filter
+	 *
+	 * @return string "is" To be an exact match
+	 */
+	public static function search_operator( $operator, $filter = array() ){
+		return 'is';
 	}
 }
 
-add_action( 'plugins_loaded', array( 'GV_Snippet___ID__', 'instance' ), 15 );
+add_filter( 'gravityview_search_operator', array( 'GV_Snippet_2730_3', 'search_operator' ), 15 );
