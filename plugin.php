fa7446995421a231e04b60ef498b2f89aacfe904
Luis Godinho <luis@gomo.pt>
2015-09-29 18:37:02 +0100

    GravityView Mod: Filter recent entries based on the entry created_by user

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name:       GravityView Mod: __description__
- * Plugin URI:        https://github.com/katzwebservices/gv-snippets/tree/__ID__
- * Description:       __description__
+ * Plugin Name:       GravityView Mod: Filter recent entries based on the entry created_by user
+ * Plugin URI:        https://github.com/katzwebservices/gv-snippets/tree/3472
+ * Description:       Using the Recent Entries widget, filters the Member History to display only the user's entries
  * Version:           1.0
  * Author:            GravityView
  * Author URI:        https://gravityview.co
@@ -15,9 +15,9 @@ if ( ! defined( 'WPINC' ) ){
 	die;
 }
 
-class GV_Snippet___ID__ {
+class GV_Snippet_3472 {
 
-	public static $ID = __ID__;
+	public static $ID = 3472;
 
 	private static $_instance = null;
 
@@ -30,8 +30,34 @@ class GV_Snippet___ID__ {
 	}
 
 	public function __construct(){
-
+		add_filter( 'gravityview/widget/recent-entries/criteria', array( $this, 'filter_work_history' ), 10, 3 );
 	}
+
+	/**
+	 * Filter the recent entries widget results to just show the work history entries related with the current user entry
+	 * Using the following values:
+	 *  - Work History View ID #262
+	 *
+	 */
+	function filter_work_history(  $criteria, $instance, $form_id ) {
+		if( !class_exists( 'GFAPI') || !function_exists('gravityview_is_single_entry') || $instance['view_id'] != '262'  ) {
+			return $criteria;
+		}
+		$entry_id = gravityview_is_single_entry();
+		if( !$entry_id ) {
+			return $criteria;
+		}
+		// get the single entry displayed (from the Members form)
+		$entry = GFAPI::get_entry( $entry_id );
+		if( empty( $entry['created_by'] ) ) {
+			return $criteria;
+		}
+		$criteria['search_criteria']['field_filters'][] = array( 'key' => 'created_by', 'value' =>  $entry['created_by'] );
+
+		return $criteria;
+	}
+
+
 }
 
-add_action( 'plugins_loaded', array( 'GV_Snippet___ID__', 'instance' ), 15 );
+add_action( 'plugins_loaded', array( 'GV_Snippet_3472', 'instance' ), 15 );
