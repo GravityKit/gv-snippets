3b84a97b432c1c69b8f69ea36771ae6167317049
Luis Godinho <luis@gomo.pt>
2016-02-29 17:28:33 +0000

    Redirect the Edit Entry Go Back link to the multiple entries view
    Redirect the Edit Entry Go Back link to the multiple entries view

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name:       GravityView Mod: __description__
- * Plugin URI:        https://github.com/katzwebservices/gv-snippets/tree/__ID__
- * Description:       __description__
+ * Plugin Name:       GravityView Mod: Redirect the Edit Entry Go Back link to the multiple entries view
+ * Plugin URI:        https://github.com/gravityview/gv-snippets/tree/addon/5101-redirect-edit-to-multiple-entries
+ * Description:       Redirects the Edit Entry Go Back and cancel link to the multiple entries view
  * Version:           1.0
  * Author:            GravityView
  * Author URI:        https://gravityview.co
@@ -15,9 +15,9 @@ if ( ! defined( 'WPINC' ) ){
 	die;
 }
 
-class GV_Snippet___ID__ {
+class GV_Snippet_5101 {
 
-	public static $ID = __ID__;
+	public static $ID = 5101;
 
 	private static $_instance = null;
 
@@ -30,8 +30,35 @@ class GV_Snippet___ID__ {
 	}
 
 	public function __construct(){
-
+		add_filter( 'gravityview/edit_entry/success', array( $this, 'gv_my_update_message' ), 10, 4 );
+		add_filter( 'gravityview/edit_entry/cancel_link', array( $this, 'gv_my_edit_cancel_link' ), 10, 4 );
 	}
+
+	/**
+	 * Change the update entry success message, including the link
+	 *
+	 * @param $message string The message itself
+	 * @param $view_id int View ID
+	 * @param $entry array The Gravity Forms entry object
+	 * @param $back_link string Url to return to the original entry
+	 */
+	function gv_my_update_message( $message, $view_id, $entry, $back_link ) {
+		$link = str_replace( 'entry/'.$entry['id'].'/', '', $back_link );
+		return 'Entry Updated. <a href="'. esc_url($link) .'">Return to the list</a>';
+	}
+
+	/**
+	 * Customise the cancel button link
+	 *
+	 * @param $back_link string
+	 *
+	 * since 1.11.1
+	 */
+	function gv_my_edit_cancel_link( $back_link, $form, $entry, $view_id ) {
+		return str_replace( 'entry/'.$entry['id'].'/', '', $back_link );
+	}
+
+
 }
 
-add_action( 'plugins_loaded', array( 'GV_Snippet___ID__', 'instance' ), 15 );
+add_action( 'plugins_loaded', array( 'GV_Snippet_5101', 'instance' ), 15 );
