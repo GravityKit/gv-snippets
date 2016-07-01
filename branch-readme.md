a5131cbb31b0495e5269be3ec2521135c2351689
Zack Katz <zack@katzwebservices.com>
2016-06-30 21:49:43 -0600

    Add Missing Fields to Gravity Forms Export

--- branch-readme.md
+++ branch-readme.md
@@ -1,3 +1,9 @@
+### Add missing fields when exporting form entries from Gravity Forms
+
+Gravity Forms doesn't include all the entry data in the the Entries export. This adds the option to export these entry values: "Is Starred", "Is Read", "Entry Status", "Is Fulfilled", "Currency", "Transaction Type", "Payment Method"
+
+__Brought to you by [GravityView](https://gravityview.co)__
+
 # How to Use
 1. Download this Plugin using the **Download ZIP** link on the right
 2. Go to your WordPress administration on the **Plugins** » **Add New**
--- plugin.php
+++ plugin.php
@@ -1,37 +1,51 @@
 <?php
 /**
- * Plugin Name:       GravityView Mod: __description__
- * Plugin URI:        https://github.com/katzwebservices/gv-snippets/tree/__ID__
- * Description:       __description__
- * Version:           1.0
- * Author:            GravityView
- * Author URI:        https://gravityview.co
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Plugin Name:       	GravityView - Add Missing Fields to Gravity Forms Export
+ * Plugin URI:        	https://github.com/katzwebservices/gv-snippets/tree/6031-add-export-fields
+ * Description:       	Gravity Forms doesn't include all the entry data in the the Entries export. This adds the option to export these entry values: "Is Starred", "Is Read", "Entry Status", "Is Fulfilled", "Currency", "Transaction Type", "Payment Method"
+ * Version:          	1.0
+ * Author:            	GravityView
+ * Author URI:        	https://gravityview.co
+ * License:           	GPLv2 or later
+ * License URI: 		http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ){
-	die;
+add_filter( 'gform_export_fields', 'gravity_forms_export_add_missing_meta_fields' );
+
+function gravity_forms_export_add_missing_meta_fields( $form = array() ) {
+
+	$missing_meta = array(
+		array(
+			'id' => 'is_starred',
+			'label' => __( 'Is Starred', 'gravityforms' ),
+		),
+		array(
+			'id' => 'is_read',
+			'label' => __( 'Is Read', 'gravityforms' ),
+		),
+		array(
+			'id' => 'status',
+			'label' => __( 'Entry Status', 'gravityforms' ),
+		),
+		array(
+			'id' => 'is_fulfilled',
+			'label' => __( 'Is Fulfilled', 'gravityforms' ),
+		),
+		array(
+			'id' => 'currency',
+			'label' => __( 'Currency', 'gravityforms' ),
+		),
+		array(
+			'id' => 'transaction_type',
+			'label' => __( 'Transaction Type', 'gravityforms' ),
+		),
+		array(
+			'id' => 'payment_method',
+			'label' => __( 'Payment Method', 'gravityforms' ),
+		),
+	);
+
+	$form['fields'] = array_merge( $form['fields'], $missing_meta );
+
+	return $form;
 }
-
-class GV_Snippet___ID__ {
-
-	public static $ID = __ID__;
-
-	private static $_instance = null;
-
-	public static function instance(){
-		if ( ! ( self::$_instance instanceof self ) ) {
-			self::$_instance = new self();
-		}
-
-		return self::$_instance;
-	}
-
-	public function __construct(){
-
-	}
-}
-
-add_action( 'plugins_loaded', array( 'GV_Snippet___ID__', 'instance' ), 15 );
