b0c859138e485602c9737fdfa7b8538d6ab21687
Zack Katz <zack@katzwebservices.com>
2016-07-05 22:49:22 -0600

    Push

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name:       GravityView Mod: __description__
- * Plugin URI:        https://github.com/katzwebservices/gv-snippets/tree/__ID__
- * Description:       __description__
+ * Plugin Name:       GravityView Mod: Disable User Registration Updates on Edit Entry
+ * Plugin URI:        https://github.com/katzwebservices/gv-snippets/tree/6065-disable-user-registration-update
+ * Description:       Prevent User details from being modified during Edit Entry.
  * Version:           1.0
  * Author:            GravityView
  * Author URI:        https://gravityview.co
@@ -15,23 +15,4 @@ if ( ! defined( 'WPINC' ) ){
 	die;
 }
 
-class GV_Snippet___ID__ {
-
-	public static $ID = __ID__;
-
-	private static $_instance = null;
-
-	public static function instance(){
-		if ( ! ( self::$_instance instanceof self ) ) {
-			self::$_instance = new self();
-		}
-
-		return self::$_instance;
-	}
-
-	public function __construct(){
-
-	}
-}
-
-add_action( 'plugins_loaded', array( 'GV_Snippet___ID__', 'instance' ), 15 );
+add_filter( 'gravityview/edit_entry/user_registration/trigger_update', '__return_false' );
